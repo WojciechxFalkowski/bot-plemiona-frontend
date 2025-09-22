7a4d281e75f156d8afe16af323232895dd8d7c61
Wojtek Falkowski <wojciechxfalkowski@gmail.com>
2025-09-22 22:49:50 +0200

    Add Firebase messaging service: create firebase-messaging-sw.js to handle background notifications using Firebase SDK, initializing messaging and displaying notifications based on received payloads.

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,28 @@
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDX0NcDxb-qvXjSRoi_styzDJSJIacWa_c",
+  authDomain: "plemiona-bot-notifica-220925.firebaseapp.com",
+  projectId: "plemiona-bot-notifica-220925",
+  storageBucket: "plemiona-bot-notifica-220925.firebasestorage.app",
+  messagingSenderId: "337582008506",
+  appId: "1:337582008506:web:e6cd3695ce464bdcda7a83",
+  measurementId: "G-EJWKV3SPB0"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/favicon.ico'
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
